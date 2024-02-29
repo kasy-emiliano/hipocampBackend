27d1020b815af0cf12d0f0d1b047bcc2641e825d
kasy-emiliano <99735371+kasy-emiliano@users.noreply.github.com>
2024-02-29 10:57:06 +0300

    code

---
+++ src/main/java/com/example/elearning/models/Customer.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private Long customerId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
